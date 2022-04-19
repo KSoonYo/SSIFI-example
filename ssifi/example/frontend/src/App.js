7a302ab1b955391f0a847654c99d123a29b08546
KimSoonYo <rlatnsdy125@naver.com>
2022-04-19 16:02:27 +0900

    fix: 🐛 [FE] components, pages 디렉토리 장소 변경
    components, pages 디렉토리 src 내부로 이동

--- ssifi/example/frontend/src/App.js
+++ ssifi/example/frontend/src/App.js
@@ -1,7 +1,12 @@
+import ChatMode from './components/ChatMode'
 import './App.css'
 
 function App() {
-  return <div></div>
+  return (
+    <div>
+      <ChatMode />
+    </div>
+  )
 }
 
 export default App
---
+++ ssifi/example/frontend/src/components/ChatMode.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChatMode = () => {
+  return (
+    <div>
+      <h1> 안녕하세요 저는 SSIFI입니다.</h1>
+    </div>
+  )
+}
+
+export default ChatMode
