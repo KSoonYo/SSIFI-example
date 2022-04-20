6a8dc690dfa9ca5d8e38d4af2e6c0747326cc3e8
pch1656 <pch1656@gmail.com>
2022-04-20 15:05:21 +0900

    feat: 🎸 [FE] 음성 대화 모드 레이아우 구현
    음성 대화 모드 레이아웃, 대화 인식 애니메이션 구현
    
    ✅ Closes: #S06P31S203-76

--- ssifi/example/frontend/src/components/VoiceMode.js
+++ ssifi/example/frontend/src/components/VoiceMode.js
@@ -1,7 +1,72 @@
-import React from 'react'
+import { Box, Button, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import '../style/SoundWave.css'
 
 const VoiceMode = () => {
-  return <div></div>
+  const [record, setRecord] = useState(false)
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+      <Box
+        sx={{
+          border: '1px solid green',
+          margin: '0 auto',
+          textAlign: 'center',
+          height: '300px',
+          mb: '80px',
+          width: '100%',
+        }}
+      >
+        AI Image
+      </Box>
+      <Box style={soundWave}>
+        {!record ? (
+          <div className="loader">
+            <span className="stroke2" />
+            <span className="stroke2" />
+            <span className="stroke2" />
+            <span className="stroke2" />
+            <span className="stroke2" />
+            <span className="stroke2" />
+            <span className="stroke2" />
+          </div>
+        ) : (
+          <div className="loader">
+            <span className="stroke" />
+            <span className="stroke" />
+            <span className="stroke" />
+            <span className="stroke" />
+            <span className="stroke" />
+            <span className="stroke" />
+            <span className="stroke" />
+          </div>
+        )}
+      </Box>
+      <Box sx={messageBox}>
+        <Typography>입력중</Typography>
+        <Button onClick={() => setRecord(!record)}>녹음</Button>
+      </Box>
+      <Button>채팅 슬라이더 버튼</Button>
+    </div>
+  )
 }
 
 export default VoiceMode
+
+// style
+const soundWave = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  height: '100px',
+}
+
+const messageBox = {
+  width: '329px',
+  height: '108px',
+  border: '1px solid red',
+  margin: '0 auto',
+  display: 'flex',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+  mt: '40px',
+}
