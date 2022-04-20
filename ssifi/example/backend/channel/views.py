cccf1b028fd3f7fd1d5b24f86450d127dbe474da
이재만 <lbefull@naver.com>
2022-04-20 16:09:47 +0900

    feat: 🎸 [BE] stt 함수에서 파일 확장자 처리
    wav 확장자가 아닐 경우 406 error 발생시키도록 로직 구성
    
    ✅ Closes: #S06P31S203-49

--- ssifi/example/backend/channel/views.py
+++ ssifi/example/backend/channel/views.py
@@ -1,5 +1,6 @@
 from django.http.response import JsonResponse
 from rest_framework.decorators import api_view
+from rest_framework import status
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings
 import os
@@ -14,13 +15,18 @@ def stt(request):
     fs = FileSystemStorage()
 
     file = request.FILES['speech']
-    filename = fs.save('input.wav', file)
+    file_extension = str(file).split('.')[-1]
+
+    if file_extension.lower() != 'wav':
+        return JsonResponse({'detail': '파일 형식이 잘못되었습니다.'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+    savename = fs.save('input.wav', file)
 
     # TODO: AI stt 모델에 음성파일을 넘겨주고 text를 return 받는 로직
     result = str(file)
-    playsound.playsound(os.path.join(settings.MEDIA_ROOT, filename))
+    playsound.playsound(os.path.join(settings.MEDIA_ROOT, savename))
 
-    os.remove(os.path.join(settings.MEDIA_ROOT, filename))
+    os.remove(os.path.join(settings.MEDIA_ROOT, savename))
     
     response = {'message': result}
     return JsonResponse(response)
