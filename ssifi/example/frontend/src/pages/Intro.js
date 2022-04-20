4ad8811a4b078952909bcac8da82e916de01469c
KimSoonYo <rlatnsdy125@naver.com>
2022-04-20 11:52:56 +0900

    feat: 🎸 [FE] Intro 컴포넌트 구현
    Intro 대화방 입구 화면 레이아웃 구현

--- ssifi/example/frontend/src/pages/Intro.js
+++ ssifi/example/frontend/src/pages/Intro.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import IconButton from '@mui/material/IconButton'
+import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined'
 
 const Intro = () => {
-  return <div></div>
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+      <div>
+        <div style={{ height: '40vh' }}>
+          <img src="assets/ssifi.png" alt="씨피" width="100%" height="100%" style={{ objectFit: 'cover' }} />
+        </div>
+        <h1>
+          <p>안녕하세요</p>
+          <p>저는 SSIFI입니다.</p>
+        </h1>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
+        <IconButton sx={{ mx: 'auto', boxShadow: 10, fontSize: 50, borderRadius: 10 }}>
+          <ArrowForwardIosOutlinedIcon />
+        </IconButton>
+      </div>
+    </div>
+  )
 }
 
 export default Intro
