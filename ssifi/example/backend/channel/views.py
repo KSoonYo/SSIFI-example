f2af2b3f4a857131b41691940ddf9634f9c263b7
이재만 <lbefull@naver.com>
2022-04-20 13:54:37 +0900

    feat: 🎸 [BE] stt 함수 초기 구성
    stt URL과 view 함수(뼈대) 연결
    
    ✅ Closes: #S06P31S203-50

--- ssifi/example/backend/channel/views.py
+++ ssifi/example/backend/channel/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http.response import JsonResponse
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view(['POST'])
+def stt(request):
+    '''
+    음성파일(.wav)를 입력받아 해당 음성의 한국어 text를 반환
+    '''
+    response = {'message': '출력 확인'}
+    return JsonResponse(response)
