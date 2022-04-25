128baad32399d06b7584aa3b6c62b57390ee31ed
KimSoonYo <rlatnsdy125@naver.com>
2022-04-25 15:12:24 +0900

    feat: 🎸 [FE] axios 설치 및 api 연동 설정
    axios 패키지 설치, api 연동 설정, reqeust 양식 설정

---
+++ ssifi/example/frontend/src/api/index.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const BASE_URL = process.env.REACT_APP_BASE_URL
+
+const createAxiosInstance = () => {
+  const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+  })
+
+  return axiosInstance
+}
+
+export const axiosInstance = createAxiosInstance()
