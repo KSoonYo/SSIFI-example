598e4e3b3937c4968ba07a3036cd5e07786ae1f1
pch1656 <pch1656@gmail.com>
2022-04-28 15:21:06 +0900

    style: 💄 [FE] mui 폰트 적용
    SPOQA HAN SANS NEO 폰트 적용

--- ssifi/example/frontend/src/index.js
+++ ssifi/example/frontend/src/index.js
@@ -4,11 +4,20 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'SpoqaHanSansNeo Bold',
+  },
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <BrowserRouter>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </BrowserRouter>,
 )
 
