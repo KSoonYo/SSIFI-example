444f3b78f65bcbae9549906c099be5a9b16d204c
KimSoonYo <rlatnsdy125@naver.com>
2022-04-29 10:34:49 +0900

    feat: 🎸 [FE] 음성 녹음 기능 구현
    음성 녹음, WAV 인코딩, 백엔드 전송

--- ssifi/example/frontend/src/components/VoiceMode.jsx
+++ ssifi/example/frontend/src/components/VoiceMode.jsx
@@ -1,5 +1,5 @@
 import { Box, Typography, Modal, Button } from '@mui/material'
-import React, { useCallback, useState } from 'react'
+import React, { useState } from 'react'
 
 import IconButton from '@mui/material/IconButton'
 import ExpandLessRoundedIcon from '@mui/icons-material/ExpandLessRounded'
@@ -9,95 +9,37 @@ import SendIcon from '@mui/icons-material/Send'
 import SoundWave from './SoundWave'
 import { postRequest } from '../api/requests'
 
+import AudioReactRecorder, { RecordState } from './AudioRecorder'
+
 const VoiceMode = () => {
   const [open, setOpen] = useState(false)
-  const [stream, setStream] = useState()
-  const [media, setMedia] = useState()
-  const [onRec, setOnRec] = useState(true)
-  const [source, setSource] = useState()
-  const [analyser, setAnalyser] = useState()
-  const [blobUrl, setBlobUrl] = useState([])
-  const [audioFile, setAudioFile] = useState()
+  const [onRec, setOnRec] = useState(false)
+  const [recordState, setRecordState] = useState('')
 
   const handleOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
-
-  const onRecAudio = () => {
-    // 음원정보를 담은 노드를 생성하거나 음원을 실행또는 디코딩 시키는 일을 한다
-    const audioCtx = new (window.AudioContext || window.webkitAudioContext)()
-    // 자바스크립트를 통해 음원의 진행상태에 직접접근에 사용된다.
-    const analyser = audioCtx.createScriptProcessor(0, 1, 1)
-    setAnalyser(analyser)
-
-    function makeSound(stream) {
-      // 내 컴퓨터의 마이크나 다른 소스를 통해 발생한 오디오 스트림의 정보를 보여준다.
-      const source = audioCtx.createMediaStreamSource(stream)
-      setSource(source)
-      source.connect(analyser)
-      analyser.connect(audioCtx.destination)
+  const handleRec = () => {
+    if (onRec === true) {
+      stop()
+    } else {
+      start()
     }
-    // 마이크 사용 권한 획득
-    navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
-      const mediaRecorder = new MediaRecorder(stream)
-      mediaRecorder.start()
-      setStream(stream)
-      setMedia(mediaRecorder)
-      makeSound(stream)
-
-      analyser.onaudioprocess = function (e) {
-        // 3분(180초) 지나면 자동으로 음성 저장 및 녹음 중지
-        if (e.playbackTime > 180) {
-          stream.getAudioTracks().forEach(function (track) {
-            track.stop()
-          })
-          mediaRecorder.stop()
-          // 메서드가 호출 된 노드 연결 해제
-          analyser.disconnect()
-          audioCtx.createMediaStreamSource(stream).disconnect()
-
-          mediaRecorder.ondataavailable = function (e) {
-            setOnRec(true)
-          }
-        } else {
-          setOnRec(false)
-        }
-      }
-    })
+    setOnRec(!onRec)
   }
 
-  const offRecAudio = () => {
-    // dataavailable 이벤트로 Blob 데이터에 대한 응답을 받을 수 있음
-    media.ondataavailable = function (e) {
-      // 중지되면 wav파일로 새로운 blob 생성
-      const newBlob = new Blob([e.data], { type: 'audio/wav' })
-      console.log(newBlob)
-      setBlobUrl(newBlob)
-      setOnRec(true)
-    }
-
-    // 모든 트랙에서 stop()을 호출해 오디오 스트림을 정지
-    stream.getAudioTracks().forEach(function (track) {
-      track.stop()
-    })
-
-    // 미디어 캡처 중지
-    media.stop()
-    // 메서드가 호출 된 노드 연결 해제
-    analyser.disconnect()
-    source.disconnect()
+  const start = () => {
+    setRecordState(RecordState.START)
+    console.log('녹음 시작!')
   }
 
-  const onSubmitAudioFile = useCallback(() => {
-    if (blobUrl) {
-      console.log(URL.createObjectURL(blobUrl)) // 출력된 링크에서 녹음된 오디오 확인 가능
-    }
-    // File 생성자를 사용해 파일로 변환
-    const sound = new File([blobUrl], 'voice.wav', { lastModified: new Date().getTime(), type: 'audio/wav' })
-    console.log(sound) // File 정보 출력
-    setAudioFile(sound)
-  }, [blobUrl])
+  const stop = () => {
+    setRecordState(RecordState.STOP)
+    console.log('녹음 중지!')
+  }
 
-  const onSendAudio = async () => {
+  const onStop = async audioData => {
+    console.log('audioData', audioData)
+    const audioFile = new File([audioData.blob], 'voice.wav', { lastModified: new Date().getTime(), type: 'audio/wav' })
     try {
       console.log(audioFile)
       // 음성 파일 formdata로 전송
@@ -112,6 +54,8 @@ const VoiceMode = () => {
 
   return (
     <div className="voiceWrapper">
+      <AudioReactRecorder state={recordState} onStop={onStop} />
+
       <Box>
         <img src="assets/ssifi2.gif" alt="씨피" width="100%" style={{ objectFit: 'cover' }} />
       </Box>
@@ -120,7 +64,7 @@ const VoiceMode = () => {
       </Box>
       <Box sx={messageBox}>
         <Typography>대기 표시</Typography>
-        <IconButton onClick={onSendAudio}>
+        <IconButton>
           <SendIcon />
         </IconButton>
       </Box>
@@ -135,10 +79,10 @@ const VoiceMode = () => {
           <ExpandLessRoundedIcon />
         </IconButton>
       </Box>
-      <Button variant="contained" onClick={onRec ? onRecAudio : offRecAudio} sx={{ backgroundColor: 'red' }}>
+      <Button variant="contained" onClick={handleRec} sx={{ backgroundColor: 'red' }}>
         <MicIcon />
       </Button>
-      <Button onClick={onSubmitAudioFile}>결과 확인</Button>
+      <Button>결과 확인</Button>
       <Modal
         open={open}
         onClose={handleClose}
