ccadccc762d9e02c76b48d86382894c00c6c04d2
이재만 <lbefull@naver.com>
2022-05-03 11:37:09 +0900

    feat: 🎸 [BE] 파일 삭제 로직 변경
    TTS로 생성된 음성 파일을 주기 삭제에서 일정 시간 후 삭제로 변경
    
    ✅ Closes: #S06P31S203-156

--- ssifi/example/backend/channel/tasks.py
+++ ssifi/example/backend/channel/tasks.py
@@ -3,14 +3,15 @@ from celery import shared_task
 from django.conf import settings
 from datetime import datetime
 import os, shutil
+import time
 
 @shared_task
-def delete_tts_file():
-    path = str(settings.MEDIA_ROOT) + '\\tts'
-    if os.path.exists(path):
-        shutil.rmtree(path)
-        print(str(datetime.now()) + ' deleted tts folder')
-    
-    if not os.path.exists(path):
-        os.makedirs(path)
-        print(str(datetime.now()) + ' maked tts folder')
+def delete_tts_file(file_name):
+    time.sleep(3600)
+
+    if os.path.isfile(f'media/tts/{file_name}'):
+        os.remove(f'media/tts/{file_name}')
+        print(str(datetime.now()) + f' {file_name} 삭제')
+
+    else:
+        print(str(datetime.now()) + f' {file_name}이 없습니다.')
