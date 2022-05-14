767be32d82876f4dc0cf74fb18ec2442c686db56
pch1656 <pch1656@gmail.com>
2022-05-15 00:12:53 +0900

    feat: 🎸 [FE] 모드 선택 버튼 슬라이더 기능 추가
    ai 모드를 마우스 클릭으로 슬라이더 선택 가능

--- frontend/src/components/ModeList.jsx
+++ frontend/src/components/ModeList.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
+import Flicking from '@egjs/react-flicking'
 import { Box, Button } from '../../node_modules/@mui/material/index'
 
 import '../style/ModeList.css'
 
 const ModeList = ({ changeAiMode }) => {
+  const [btnIdx, setBtnIdx] = useState(1)
+
   const modeList = [
     { name: '소설', mode: 'novel' },
     { name: '심리상담', mode: 'wellness' },
@@ -19,31 +22,30 @@ const ModeList = ({ changeAiMode }) => {
     { name: '뉴스', mode: 'news' },
   ]
 
-  const [btnIdx, setBtnIdx] = useState(1)
-
   return (
     <Box className="mode-list">
-      {modeList.map((list, index) => (
-        <Button
-          className={index === btnIdx ? 'mode-btn-selected' : 'mode-btn'}
-          key={index}
-          onClick={() => {
-            setBtnIdx(index)
-            changeAiMode(list.mode)
-          }}
-          sx={{
-            color: 'white',
-            borderRadius: '10px',
-            border: '1px solid white',
-            mr: '10px',
-            fontFamily: 'SpoqaHanSansNeo Regular',
-          }}
-        >
-          {list.name}
-        </Button>
-      ))}
+      <Flicking align="prev" circular={true}>
+        {modeList.map((list, index) => (
+          <Button
+            className={index === btnIdx ? 'mode-btn-selected' : 'mode-btn'}
+            key={index}
+            onClick={() => {
+              setBtnIdx(index)
+              changeAiMode(list.mode)
+            }}
+            sx={{
+              color: 'white',
+              borderRadius: '10px',
+              border: '1px solid white',
+              mr: '10px',
+              fontFamily: 'SpoqaHanSansNeo Regular',
+            }}
+          >
+            {list.name}
+          </Button>
+        ))}
+      </Flicking>
     </Box>
   )
 }
-
 export default ModeList
