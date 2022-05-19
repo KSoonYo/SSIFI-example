da249445d5e34f932399a93f43b198db657bc82d
pch1656 <pch1656@gmail.com>
2022-05-19 10:58:34 +0900

    fix: 🐛 [FE] 채팅용 ai모드 사용시 음성 기능 버튼 숨기기
    심리상담모드 이외의 모드는 플레이 버튼 숨김 처리

--- frontend/src/components/InfoDialog.jsx
+++ frontend/src/components/InfoDialog.jsx
@@ -36,11 +36,11 @@ const InfoDialog = ({ open, handleClose, navigate }) => {
       <DialogTitle id="alert-dialog-title">{'정보 제공에 동의하시겠습니까?'}</DialogTitle>
       <DialogContent>
         <DialogContentText id="alert-dialog-description">
-          동의함을 누르시면 SSIFI는 더 나은 학습을 위해 <br></br>
-          사용자의 대화 내용을 보관 및 활용할 수 있습니다. <br></br>
-          동의하지 않으시면 DB에 채팅 내용이 저장되지 않습니다. <br></br>
-          *동의 여부와 상관없이 서비스를 이용하실 수 있습니다. <br></br>
-          여러분의 채팅 내용은 DB에 암호화되어 안전하게 저장됩니다
+          동의함을 누르시면 SSIFI는 더 나은 학습을 위해 <br />
+          사용자의 대화 내용을 보관 및 활용할 수 있습니다. <br />
+          동의하지 않으시면 DB에 채팅 내용이 저장되지 않습니다. <br />
+          여러분의 채팅 내용은 DB에 암호화되어 안전하게 저장됩니다 <br />
+          *동의 여부와 상관없이 서비스를 이용하실 수 있습니다.
         </DialogContentText>
       </DialogContent>
       <DialogActions>
